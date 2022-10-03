142883d79f414622beabd7ccb10c7e60ebed4140
Magnus Jerre <magnusjerre@users.noreply.github.com>
2022-10-03 17:36:33 +0200

    Add more sample data

--- src/jvmMain/resources/db/migration/V1_0_1__add_sample_data.sql
+++ src/jvmMain/resources/db/migration/V1_0_1__add_sample_data.sql
@@ -1,1 +1,27 @@
-INSERT INTO public.admin_user(email, name, password_digest, username) VALUES ( 'mangekamp@no.experis.com', 'Mange Kamp', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', 'mangekamp');
+INSERT INTO public.admin_user(email, name, password_digest, username) VALUES ( 'admin@mangekamp.no', 'Mange Kamp', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', 'admin');
+
+INSERT INTO public.category(color, name) VALUES ('#56A3A6', 'Kondisjon');
+INSERT INTO public.category(color, name) VALUES ('#6B0504', 'Balløvelser');
+INSERT INTO public.category(color, name) VALUES ('#A3320B', 'Teknikk');
+
+INSERT INTO public.person(name, email, gender, retired) VALUES ('Donal Duck', 'donald.duck@mangekamp.no', 0, false);
+INSERT INTO public.person(name, email, gender, retired) VALUES ('Dolly Duck', 'dolly.duck@mangekamp.no', 1, false);
+INSERT INTO public.person(name, email, gender, retired) VALUES ('Onkel Skrue', 'onkel.skrue@mangekamp.no', 0, false);
+
+
+INSERT INTO public.season(name, start_year) VALUES ('2022-2023 Oslo', 2022);
+
+INSERT INTO public.event(date, title, venue, category_id, season_id) VALUES ('2022-08-25', 'Minigolf', 'Andeby minigolfpark', 2, 1);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(1, '40', 1, 1);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(1, '38', 1, 2);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(2, '43', 1, 3);
+
+INSERT INTO public.event(date, title, venue, category_id, season_id) VALUES ('2022-09-06', 'Orientering', 'Andebyskogen', 1, 1);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(1, '12:00', 2, 1);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(1, '13:00', 2, 2);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(2, '14:00', 2, 3);
+
+INSERT INTO public.event(date, title, venue, category_id, season_id) VALUES ('2022-09-06', 'Bueskyting', 'Andeby andejaktklubb', 1, 1);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(2, '30', 3, 1);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(1, '33', 3, 2);
+INSERT INTO public.participant(rank, score, event_id, person_id) VALUES(1, '45', 3, 3);
