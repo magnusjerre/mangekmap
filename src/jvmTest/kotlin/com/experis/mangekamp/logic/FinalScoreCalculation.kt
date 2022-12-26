cae0df82e3554969415845ddca6bd107815c0d58
Magnus Jerre <magnusjerre@users.noreply.github.com>
2022-12-26 21:46:14 +0100

    wip2

--- src/jvmTest/kotlin/com/experis/mangekamp/logic/FinalScoreCalculation.kt
+++ src/jvmTest/kotlin/com/experis/mangekamp/logic/FinalScoreCalculation.kt
@@ -22,30 +22,30 @@ class FinalScoreCalculation {
                 ONKEL_SKRUE,
             ),
             events = listOf(
-                SetupEvent("Minigolf", SetupCategory.BALL)
+                minigolf()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2)
                     .add(ONKEL_SKRUE, 3)
                     .add(DOLE, 4)
                     .add(DOFFEN, 5),
-                SetupEvent("Orientering", SetupCategory.KONDISJON)
+                orientering()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2)
                     .add(ONKEL_SKRUE, 3)
                     .add(DOLE, 4)
                     .add(DOFFEN, 5),
-                SetupEvent("Bordtennis", SetupCategory.BALL)
+                bordtennis()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2)
                     .add(ONKEL_SKRUE, 3)
                     .add(DOLE, 4)
                     .add(DOFFEN, 5),
-                SetupEvent("Frisbeegolf", SetupCategory.TEKNIKK)
+                frisbeegolf()
                     .add(DONALD_DUCK, 1)
                     .add(ONKEL_SKRUE, 2)
                     .add(DOLE, 3)
                     .addAttendanceOnly(DOFFEN),
-                SetupEvent("Roing", SetupCategory.KONDISJON)
+                roing()
                     .add(DONALD_DUCK, 1)
                     .addAttendanceOnly(DOLE)
             )
@@ -57,7 +57,7 @@ class FinalScoreCalculation {
             expectedMangekjemperEvents = season.mangekjemperRequiredEvents.toInt()
         ) { it.events.isMangekjemper(season.mangekjemperRequiredEvents.toInt())}
         val winner = result[0]
-        winner.shouldHave(name = "Donald Duck", seasonRank = 1, seasonPoints = 4, mangekjemperStatus = true)
+        winner.shouldHave(name = DONALD_DUCK.name, seasonRank = 1, seasonPoints = 4, mangekjemperStatus = true)
         winner.shouldHaveMangekjemperRanks(listOf(1.b, 1.k, 1.b, 1.t, 1.t), season.events)
         val second = result[1]
         second.shouldHave(name = "Onkel Skrue", seasonRank = 2, seasonPoints = 8, mangekjemperStatus = true)
@@ -87,48 +87,48 @@ class FinalScoreCalculation {
                 ONKEL_SKRUE,
             ),
             events = listOf(
-                SetupEvent("Minigolf", SetupCategory.BALL)
+                minigolf()
                     .add(DONALD_DUCK, 1)
                     .add(ONKEL_SKRUE, 2)
                     .add(DOFFEN, 3)
                     .add(OLE, 4)
                     .add(DOLE, 5),
-                SetupEvent("Orientering", SetupCategory.KONDISJON)
+                orientering()
                     .add(ONKEL_SKRUE, 1)
                     .add(DONALD_DUCK, 2)
                     .add(OLE, 3)
                     .add(DOFFEN, 4),
-                SetupEvent("Bordtennis", SetupCategory.BALL)
+                bordtennis()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2)
                     .add(DOFFEN, 3)
                     .add(DOLE, 4),
-                SetupEvent("Frisbeegolf", SetupCategory.TEKNIKK)
-                    .add(DOFFEN, 1,)
+                frisbeegolf()
+                    .add(DOFFEN, 1)
                     .add(DONALD_DUCK, 2)
                     .add(OLE, 3)
                     .add(DOLE, 4),
-                SetupEvent("Roing", SetupCategory.KONDISJON)
+                roing()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2)
                     .add(ONKEL_SKRUE, 3)
                     .add(DOFFEN, 4)
                     .add(DOLE, 5),
-                SetupEvent("Poker", SetupCategory.TEKNIKK)
+                poker()
                     .add(OLE, 1)
                     .add(DONALD_DUCK, 2)
                     .add(DOFFEN, 3)
                     .add(DOLE, 4),
-                SetupEvent("E-sport", SetupCategory.TEKNIKK)
+                esport()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2)
                     .add(DOFFEN, 3)
                     .add(DOLE, 4),
-                SetupEvent("Tennis Double", SetupCategory.BALL)
-                    .addTeam(1, null, DONALD_DUCK, DOFFEN)
-                    .addTeam(2, null, OLE, ONKEL_SKRUE)
-                    .addTeam(3, null, DOLE),
-                SetupEvent("Padel", SetupCategory.BALL)
+                tennisDouble()
+                    .addTeam(1, DONALD_DUCK, DOFFEN)
+                    .addTeam(2, OLE, ONKEL_SKRUE)
+                    .addTeam(3, DOLE),
+                padel()
                     .add(DONALD_DUCK, 1)
                     .add(DOFFEN, 2)
                     .add(OLE, 3)
@@ -143,7 +143,7 @@ class FinalScoreCalculation {
         ) { it.events.isMangekjemper(season.mangekjemperRequiredEvents.toInt())}
 
         val winner = result[0]
-        winner.shouldHave(name = "Donald Duck", seasonRank = 1, seasonPoints = 10, mangekjemperStatus = true)
+        winner.shouldHave(name = DONALD_DUCK.name, seasonRank = 1, seasonPoints = 10, mangekjemperStatus = true)
         winner.shouldHaveMangekjemperRanks(listOf(1.b, 1.k, 1.b, 2.t, 1.k, 2.t, 1.t, 1.b, 1.b), season.events)
         val second = result[1]
         second.shouldHave(name = "Ole", seasonRank = 2, seasonPoints = 17, mangekjemperStatus = true)
@@ -179,17 +179,17 @@ class FinalScoreCalculation {
             actualPersons = persons,
             eventIdGenerator = idGenerator,
             events = listOf(
-                SetupEvent("Minigolf", SetupCategory.BALL)
+                minigolf()
                     .add(DONALD_DUCK, 1),
-                SetupEvent("Orientering", SetupCategory.KONDISJON)
+                orientering()
                     .add(DONALD_DUCK, 1)
                     .add(DOLE, 1)
                     .add(OLE, 3)
                     .add(DOFFEN, 3),
-                SetupEvent("Bordtennis", SetupCategory.BALL)
+                bordtennis()
                     .add(DONALD_DUCK, 1)
                     .add(DOFFEN, 2),
-                SetupEvent("Frisbeegolf", SetupCategory.TEKNIKK)
+                frisbeegolf()
                     .add(DONALD_DUCK, 1)
                     .add(OLE, 2),
             )
@@ -203,10 +203,10 @@ class FinalScoreCalculation {
             actualPersons = persons,
             eventIdGenerator = idGenerator,
             events = listOf(
-                SetupEvent("Roing", SetupCategory.KONDISJON)
+                roing()
                     .add(DOLE, 1)
                     .add(OLE, 2),
-                SetupEvent("Poker", SetupCategory.TEKNIKK)
+                poker()
                     .add(DOLE, 1),
             )
         )
@@ -219,13 +219,13 @@ class FinalScoreCalculation {
             actualPersons = persons,
             eventIdGenerator = idGenerator,
             events = listOf(
-                SetupEvent("E-sport", SetupCategory.TEKNIKK)
+                esport()
                     .add(OLE, 1)
                     .add(DOFFEN, 2),
-                SetupEvent("Tennis Double", SetupCategory.BALL)
-                    .addTeam(1, null, DOLE)
-                    .addTeam(2, null, DOFFEN),
-                SetupEvent("Padel", SetupCategory.BALL)
+                tennisDouble()
+                    .addTeam(1, DOLE)
+                    .addTeam(2, DOFFEN),
+                padel()
                     .add(DOFFEN, 1),
             )
         )
@@ -238,35 +238,35 @@ class FinalScoreCalculation {
             expectedMangekjemperEvents = season.mangekjemperRequiredEvents.toInt()
         ) { it.events.isMangekjemper(season.mangekjemperRequiredEvents.toInt())}
 
-        val donaldMain = resultsMain.find { it.personName == "Donald Duck" }!!
-        donaldMain.shouldHave(name = "Donald Duck", seasonRank = 1, seasonPoints = 4, mangekjemperStatus = true)
+        val donaldMain = resultsMain.find { it.personName == DONALD_DUCK.name }!!
+        donaldMain.shouldHave(name = DONALD_DUCK.name, seasonRank = 1, seasonPoints = 4, mangekjemperStatus = true)
         donaldMain.shouldHaveMangekjemperRanks(listOf(1.b, 1.k, 1.b, 1.t), season.events)
-        donaldMain.events.find { it.eventName == "Minigolf" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        donaldMain.events.find { it.eventName == "Orientering" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        donaldMain.events.find { it.eventName == "Bordtennis" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        donaldMain.events.find { it.eventName == "Frisbeegolf" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        val oleMain = resultsMain.find { it.personName == "Ole" }!!
+        donaldMain.getEvent(MINIGOLF).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        donaldMain.getEvent(ORIENTERING).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        donaldMain.getEvent(BORDTENNIS).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        donaldMain.getEvent(FRISBEEGOLF).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        val oleMain = resultsMain.getParticipant(OLE)!!
         oleMain.shouldHave(name = "Ole", seasonRank = 3, seasonPoints = 37, mangekjemperStatus = false)
         oleMain.shouldHaveMangekjemperRanks(listOf(0.b, 0.k, 0.b, 0.t), season.events)
-        oleMain.events.find { it.eventName == "Orientering" }!!.eventPointsReason shouldBe PointsReason.NOT_MANGEKJEMPER
-        oleMain.events.find { it.eventName == "Frisbeegolf" }!!.eventPointsReason shouldBe PointsReason.NOT_MANGEKJEMPER
-        oleMain.events.find { it.eventName == "Roing" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_NOT_MANGEKJEMPER
-        oleMain.events.find { it.eventName == "E-sport" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_NOT_MANGEKJEMPER
+        oleMain.getEvent(ORIENTERING).eventPointsReason shouldBe PointsReason.NOT_MANGEKJEMPER
+        oleMain.getEvent(FRISBEEGOLF).eventPointsReason shouldBe PointsReason.NOT_MANGEKJEMPER
+        oleMain.getEvent(ROING).eventPointsReason shouldBe PointsReason.OTHER_REGION_NOT_MANGEKJEMPER
+        oleMain.getEvent(ESPORT).eventPointsReason shouldBe PointsReason.OTHER_REGION_NOT_MANGEKJEMPER
         oleMain.seasonPenaltyPoints shouldBe null
-        val doleMain = resultsMain.find { it.personName == "Dole" }!!
+        val doleMain = resultsMain.getParticipant(DOLE)!!
         doleMain.shouldHave(name = "Dole", seasonRank = 4, seasonPoints = 10, mangekjemperStatus = true)
         doleMain.shouldHaveMangekjemperRanks(listOf(0.b, 1.k, 0.b, 0.t), season.events)
-        doleMain.events.find { it.eventName == "Orientering" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        doleMain.events.find { it.eventName == "Roing" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
-        doleMain.events.find { it.eventName == "Poker" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
-        doleMain.events.find { it.eventName == "Tennis Double" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
-        val doffenMain = resultsMain.find { it.personName == "Doffen" }!!
+        doleMain.getEvent(ORIENTERING).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        doleMain.getEvent(ROING).eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
+        doleMain.getEvent(POKER).eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
+        doleMain.getEvent(TENNIS_DOUBLE).eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
+        val doffenMain = resultsMain.getParticipant(DOFFEN)!!
         doffenMain.shouldHave(name = "Doffen", seasonRank = 2, seasonPoints = 11, mangekjemperStatus = true)
-        doffenMain.events.find { it.eventName == "Orientering" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        doffenMain.events.find { it.eventName == "Bordtennis" }!!.eventPointsReason shouldBe PointsReason.MANGEKJEMPER
-        doffenMain.events.find { it.eventName == "E-sport" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
-        doffenMain.events.find { it.eventName == "Tennis Double" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
-        doffenMain.events.find { it.eventName == "Padel" }!!.eventPointsReason shouldBe PointsReason.OTHER_REGION_NOT_INCLUDED
+        doffenMain.getEvent(ORIENTERING).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        doffenMain.getEvent(BORDTENNIS).eventPointsReason shouldBe PointsReason.MANGEKJEMPER
+        doffenMain.getEvent(ESPORT).eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
+        doffenMain.getEvent(TENNIS_DOUBLE).eventPointsReason shouldBe PointsReason.OTHER_REGION_MANGEKJEMPER
+        doffenMain.getEvent(PADEL).eventPointsReason shouldBe PointsReason.OTHER_REGION_NOT_INCLUDED
 
         val resultsOther1 = allEvents.calculateSeason(
             seasonId = seasonOtherRegion1.id!!,
@@ -275,14 +275,14 @@ class FinalScoreCalculation {
         ) {
             it.events.isMangekjemper(season.mangekjemperRequiredEvents.toInt())
         }
-        resultsOther1.find { it.personName == "Donald Duck" } shouldBe null
-        val oleOther1 = resultsOther1.find { it.personName == "Ole" }!!
+        resultsOther1.find { it.personName == DONALD_DUCK.name } shouldBe null
+        val oleOther1 = resultsOther1.getParticipant(OLE)!!
         oleOther1.shouldHave(name = "Ole", seasonRank = 2, seasonPoints = 50, mangekjemperStatus = false)
         oleOther1.shouldHaveMangekjemperRanks(listOf(0.k, 0.t), seasonOtherRegion1.events)
-        val doleOther1 = resultsOther1.find { it.personName == "Dole" }!!
+        val doleOther1 = resultsOther1.getParticipant(DOLE)!!
         doleOther1.shouldHave(name = "Dole", seasonRank = 1, seasonPoints = 4, mangekjemperStatus = true)
         doleOther1.shouldHaveMangekjemperRanks(listOf(1.k, 1.t), seasonOtherRegion1.events)
-        resultsOther1.find { it.personName == "Doffen" } shouldBe null
+        resultsOther1.getParticipant(DOFFEN) shouldBe null
 
         val resultsOther2 = allEvents.calculateSeason(
             seasonId = seasonOtherRegion2.id!!,
@@ -291,14 +291,14 @@ class FinalScoreCalculation {
         ) {
             it.events.isMangekjemper(season.mangekjemperRequiredEvents.toInt())
         }
-        resultsOther2.find { it.personName == "Donald Duck" } shouldBe null
-        val oleOther2 = resultsOther2.find { it.personName == "Ole" }!!
+        resultsOther2.getParticipant(DONALD_DUCK) shouldBe null
+        val oleOther2 = resultsOther2.getParticipant(OLE)!!
         oleOther2.shouldHave(name = "Ole", seasonRank = 3, seasonPoints = 49, mangekjemperStatus = false)
         oleOther2.shouldHaveMangekjemperRanks(listOf(0.t, 0.b, 0.b), seasonOtherRegion2.events)
-        val doleOther2 = resultsOther2.find { it.personName == "Dole" }!!
+        val doleOther2 = resultsOther2.getParticipant(DOLE)!!
         doleOther2.shouldHave(name = "Dole", seasonRank = 2, seasonPoints = 7, mangekjemperStatus = true)
         doleOther2.shouldHaveMangekjemperRanks(listOf(0.t, 1.b, 0.b), seasonOtherRegion2.events)
-        val doffenOther2 = resultsOther2.find { it.personName == "Doffen" }!!
+        val doffenOther2 = resultsOther2.getParticipant(DOFFEN)!!
         doffenOther2.shouldHave(name = "Doffen", seasonRank = 1, seasonPoints = 6, mangekjemperStatus = true)
         doffenOther2.shouldHaveMangekjemperRanks(listOf(1.t, 2.b, 1.b), seasonOtherRegion2.events)
     }
@@ -324,4 +324,7 @@ class FinalScoreCalculation {
     private val Int.b: Int get() = this
     private val Int.k: Int get() = this
     private val Int.t: Int get() = this
+
+    private fun SeasonParticipant.getEvent(eventName: String): SeasonSimplifiedEvent = events.find { it.eventName == eventName }!!
+    private fun List<SeasonParticipant>.getParticipant(person: SetupPerson): SeasonParticipant? = find { it.personName == person.name }
 }
--- src/jvmTest/kotlin/com/experis/mangekamp/logic/Utils.kt
+++ src/jvmTest/kotlin/com/experis/mangekamp/logic/Utils.kt
@@ -19,32 +19,10 @@ enum class SetupCategory(val prettyName: String) {
 
 data class SetupEvent(val name: String, val category: SetupCategory) {
     val participants: MutableList<SetupParticipant> = mutableListOf()
-    val participations: MutableList<SetupParticipation> = mutableListOf()
-    fun add(participant: SetupParticipant): SetupEvent {
-        participants.add(participant)
-        participations.add(SetupParticipation(participant))
-        return this
-    }
 
-    fun addAttendanceOnly(name: String): SetupEvent = apply {
-        participants.add(SetupParticipant(name, null, -1, null, true))
-        participations.add(SetupParticipation(SetupParticipant(name, null, -1, null, true)))
-    }
-
-    fun addTeam(rank: Int, score: String?, vararg names: String): SetupEvent = apply {
+    fun addTeam(rank: Int, vararg setupPersons: SetupPerson): SetupEvent = apply {
         val teamNumber = (participants.lastOrNull()?.teamNumber ?: 0) + 1
-        names.forEach { participants.add(SetupParticipant(it, null, rank, score, false, teamNumber)) }
-    }
-
-    fun addTeam(rank: Int, score: String?, vararg setupPersons: SetupPerson): SetupEvent = apply {
-        val teamNumber = (participants.lastOrNull()?.teamNumber ?: 0) + 1
-        setupPersons.forEach { participants.add(SetupParticipant(it.name, it.gender, rank, score, false, teamNumber)) }
-    }
-
-    fun add(name: String, rank: Int, score: String? = null): SetupEvent {
-        participants.add(SetupParticipant(name, null, rank, score))
-        participations.add(SetupParticipation(SetupParticipant(name, null, rank, score)))
-        return this
+        setupPersons.forEach { participants.add(SetupParticipant(it.name, it.gender, rank, null, false, teamNumber)) }
     }
 
     fun add(person: SetupPerson, rank: Int, score: String? = null): SetupEvent = apply {
@@ -53,12 +31,6 @@ data class SetupEvent(val name: String, val category: SetupCategory) {
 
     fun addAttendanceOnly(person: SetupPerson): SetupEvent = apply {
         participants.add(SetupParticipant(person.name, person.gender, -1, null, true))
-    }
-}
-
-class SetupParticipation(val participants: MutableList<SetupParticipant> = mutableListOf()) {
-    constructor(vararg participants: SetupParticipant) : this() {
-        this.participants.addAll(participants)
     }
 }
 
@@ -72,42 +44,6 @@ data class SetupParticipant(
 )
 
 data class SetupPerson(val name: String, val gender: Gender, val id: Long)
-
-val events = listOf(
-    SetupEvent("Minigolf", SetupCategory.BALL)
-        .add("Donald Duck", 1, "jdjd")
-        .add("Dolly Duck", 1, null)
-        .addAttendanceOnly("Onkel Skrue")
-        .addTeam(2, score = "jfjf", "Winnie", "The", "Pooh")
-)
-
-val persons = listOf(
-    SetupPerson("Donald Duck", Gender.MALE, 1),
-    SetupPerson("Dolly Duck", Gender.FEMALE, 2)
-)
-
-val SEASON = setupSeason(
-    "2022-20223", Region.OSLO, 4,
-    listOf(
-        SetupPerson("Donald Duck", Gender.MALE, 1),
-        SetupPerson("Onkel Skrue", Gender.MALE, 2),
-        SetupPerson("Langbein", Gender.MALE, 3),
-        SetupPerson("Dolly Duck", Gender.FEMALE, 4),
-    ),
-    listOf(
-        SetupEvent("Minigolf", SetupCategory.BALL)
-            .add("Donald Duck", 1, "56")
-            .add("Onkel Skrue", 2, "60")
-            .add("Dolly Duck", 1),
-        SetupEvent("Volleyball", SetupCategory.BALL)
-            .addTeam(1, "3 seiere", "Donald Duck", "Langbein")
-            .addTeam(2, "3 tap", "Onkel Skrue", "Dolly Duck"),
-        SetupEvent("Orientering", SetupCategory.KONDISJON)
-            .add("Donald Duck", 1, "3 min")
-            .add("Langbein", 2, "6 min")
-            .addAttendanceOnly("Onkel Skrue"),
-    )
-)
 
 fun List<SetupPerson>.toActualPersons(): List<Person> = map {
     Person(name = it.name, email = it.name, gender = it.gender, retired = false, id = it.id).apply {
@@ -216,3 +152,22 @@ val OLE = SetupPerson("Ole", Gender.MALE, 3)
 val DOLE = SetupPerson("Dole", Gender.MALE, 2)
 val DOFFEN = SetupPerson("Doffen", Gender.MALE, 4)
 val ONKEL_SKRUE = SetupPerson("Onkel Skrue", Gender.MALE, 5)
+
+val MINIGOLF = "Minigolf"
+val ORIENTERING = "Orientering"
+val BORDTENNIS = "Bordtennis"
+val FRISBEEGOLF = "Frisbeegolf"
+val ROING = "Roing"
+val POKER = "Poker"
+val ESPORT = "E-sport"
+val TENNIS_DOUBLE = "Tennis Double"
+val PADEL = "Padel"
+fun minigolf() = SetupEvent(MINIGOLF, SetupCategory.BALL)
+fun orientering() = SetupEvent(ORIENTERING, SetupCategory.KONDISJON)
+fun bordtennis() = SetupEvent(BORDTENNIS, SetupCategory.BALL)
+fun frisbeegolf() = SetupEvent(FRISBEEGOLF, SetupCategory.TEKNIKK)
+fun roing() = SetupEvent(ROING, SetupCategory.KONDISJON)
+fun poker() = SetupEvent(POKER, SetupCategory.TEKNIKK)
+fun esport() = SetupEvent(ESPORT, SetupCategory.TEKNIKK)
+fun tennisDouble() = SetupEvent(TENNIS_DOUBLE, SetupCategory.BALL)
+fun padel() = SetupEvent(PADEL, SetupCategory.BALL)
